0dab688cade49e21ee273557ee8d6800059a1172
jaimegarciareyes <jaimegarciareyes@LEM231-16>
2023-01-18 19:48:22 -0800

    Add project files.

---
+++ COMP003A.Assignment01/Program.cs
@@ -0,0 +1,115 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * Author: Jaime Garcia Reyes
+ * Course: COMP-003A
+ * Purpose: first week assignment
+ */
+namespace COMP003A.Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. My name is Jaime Garcia");
+            Console.WriteLine("2. I live in Fresno");
+            Console.WriteLine("3. I am a mechanical engineer major");
+            Console.WriteLine("5. I have a dog named Bailey");
+            Console.WriteLine("6. I have a dog named Zeldris");
+            Console.WriteLine("7. I have a 2016 Ford Mustang");
+            Console.WriteLine("8. I have coded HTML/CSS before");
+            Console.WriteLine("9. I am a size 10 in shoes");
+            Console.WriteLine("10. I like pizza");
+            Console.WriteLine("11. I like strawberries");
+            Console.WriteLine("12. I am transferring fall 20213");
+            Console.WriteLine("13. I am wearing green today");
+            Console.WriteLine("14. I work at T-Mobile");
+            Console.WriteLine("15. I drove to class today");
+            Console.WriteLine("16. I ate Chipotle for lunch");
+            Console.WriteLine("17. I have brown eyes");
+            Console.WriteLine("18. I have black hair");
+            Console.WriteLine("19. I am 5'8");
+            Console.WriteLine("20. I used to live in Lemoore");
+            Console.WriteLine("21. I used to live in Huron");
+            Console.WriteLine("22. I used to live in Utah");
+            Console.WriteLine("23. I want to move to San Diego");
+            Console.WriteLine("24. I want to travel to Europe");
+            Console.WriteLine("25. I want to travel to Egypt");
+            Console.WriteLine("26. I want to go to Tokyo");
+            Console.WriteLine("27. I want a Rottweiler dog");
+            Console.WriteLine("28. I want a Ford Mustang Shelby GT350");
+            Console.WriteLine("29. I like bananas");
+            Console.WriteLine("30. I like to drink water");
+            Console.WriteLine("31. I like coffee");
+            Console.WriteLine("32. I like to play soccer");
+            Console.WriteLine("33. I play soccer every Sunday");
+            Console.WriteLine("34. I got an Oculus 2 for Christmas");
+            Console.WriteLine("35. I applied to Fresno State");
+            Console.WriteLine("36. I applied to Sacramento State");
+            Console.WriteLine("37. I applied to Cal State Northridge");
+            Console.WriteLine("38. I originally attended UC Riverside");
+            Console.WriteLine("39. I have a family dog named Canelo");
+            Console.WriteLine("40. I like The Office");
+            Console.WriteLine("41. I liked The Good Place");
+            Console.WriteLine("42. I like Shrek");
+            Console.WriteLine("43. I would like to go to Disneyland");
+            Console.WriteLine("44. I would like to earn my engineering degree");
+            Console.WriteLine("45. I want to learn in this class");
+            Console.WriteLine("46. I also have a Jeep");
+            Console.WriteLine("47. I have been in a relationship for 9 years");
+            Console.WriteLine("48. I like to code");
+            Console.WriteLine("49. I can type without looking");
+            Console.WriteLine("50. I like to drink Sprite");
+            Console.WriteLine("51. I like to eat teppanyaki");
+            Console.WriteLine("52. I like to eat burgers");
+            Console.WriteLine("53. I like Christmas");
+            Console.WriteLine("54. I like Halloween");
+            Console.WriteLine("56. I like to drive");
+            Console.WriteLine("57. I went to Utah last month");
+            Console.WriteLine("58. I went to Hawaii 1 year ago");
+            Console.WriteLine("59. I went to Miami 1 year ago");
+            Console.WriteLine("60. I went to Las Vegas 1 year ago");
+            Console.WriteLine("61. My birthday is in August");
+            Console.WriteLine("62. I went to Coalinga High School");
+            Console.WriteLine("63. I attended Fresno City College");
+            Console.WriteLine("64. I first flew by myself when I was 18");
+            Console.WriteLine("65. My parents are Mexican");
+            Console.WriteLine("66. My brother lives in San Diego");
+            Console.WriteLine("67. My grandma lives in Mexico");
+            Console.WriteLine("68. I want to own an engineering firm");
+            Console.WriteLine("69. I used to work at CarMax");
+            Console.WriteLine("70. I used to work at MetroPCS");
+            Console.WriteLine("71. I own an iPhone");
+            Console.WriteLine("72. I like Pokemon");
+            Console.WriteLine("73. I like Dragonball Z");
+            Console.WriteLine("74. I like to watch Shameless");
+            Console.WriteLine("75. I like to watch New Girl");
+            Console.WriteLine("76. My favorite soccer player is Messi");
+            Console.WriteLine("77. I have a younger brother");
+            Console.WriteLine("78. I used to live in Riverside");
+            Console.WriteLine("79. I like Korean BBQ");
+            Console.WriteLine("80. I like to sleep in");
+            Console.WriteLine("81. I like to play NBA 2k");
+            Console.WriteLine("82. I like to play FIFA");
+            Console.WriteLine("83. I liked the 2022 FIFA World Cup");
+            Console.WriteLine("84. I have attended West Hills in the past");
+            Console.WriteLine("85. I am 25 years old");
+            Console.WriteLine("86. I am turning 26 this year");
+            Console.WriteLine("87. My zodiac sign is a Leo");
+            Console.WriteLine("88. I have 2 tattoos");
+            Console.WriteLine("89. Both tattoos were done in Lemoore");
+            Console.WriteLine("90. I am wearing black jeans");
+            Console.WriteLine("91. I am wearing black shoes");
+            Console.WriteLine("92. I am sitting next to someone");
+            Console.WriteLine("93. I am doing my assignment");
+            Console.WriteLine("94. I like to eat Indian food");
+            Console.WriteLine("95. I like to drink Ciroc Vodka");
+            Console.WriteLine("96. I like to go out on weekends");
+            Console.WriteLine("97. I don't like to wear hats");
+            Console.WriteLine("98. I don't like to wear beanies");
+            Console.WriteLine("99. I get a low fade when I cut my hair");
+            Console.WriteLine("100. My drive here was 45 min");
+
+        }
+    }
+}
